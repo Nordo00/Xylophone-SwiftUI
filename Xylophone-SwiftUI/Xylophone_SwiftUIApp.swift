f7c43d12b7de9565c939df3e213de29a8e129417
Nordo00 <ollie.a.lin@gmail.com>
2023-03-02 10:22:14 -0500

    Initial Commit

---
+++ Xylophone-SwiftUI/Xylophone_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xylophone_SwiftUIApp.swift
+//  Xylophone-SwiftUI
+//
+//  Created by Oliver Lin on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Xylophone_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
